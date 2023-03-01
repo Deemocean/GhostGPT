96ab58e916e3778fd273f867176bf747a0b7fb98
Deemo Chen <yc938@cornell.edu>
2023-03-01 17:12:21 -0500

    Upload alpha version

---
+++ ghost.py
@@ -0,0 +1,41 @@
+import openai
+import sys
+
+#PUT YOUR OPENAI API KEY HERE:
+openai.api_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+imprint = sys.argv[1]
+#imprint = "test"
+print("\nInjecting Nerual Imprint: "+ str(imprint)+" ...")
+
+imprint_path = "IMPRINTS/"+imprint+".ni"
+
+imprint_file =open(imprint_path)
+chat_history = eval(imprint_file.read())
+imprint_file.close()
+
+def history_add(history, role, content):
+    history.append({"role": role, "content": content})
+    return history
+
+
+def chat(history,content):
+    unanswered_history = history_add(history,"user",content)
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=unanswered_history)
+    msg = response['choices'][0]['message']['content']
+    history_add(history, "assistant",msg)
+    save(history,imprint_path)
+    return msg
+
+def save(history, path):
+    nifile = open(path, "w")
+    nifile.write(str(history))
+    nifile.close()
+
+usr_input=""
+while (usr_input!="exit"):
+    usr_input=input("YOU: ")
+    if(usr_input!="exit"):
+      print("GHOST: "+chat(chat_history, usr_input))
---
+++ ghost.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+echo ' ________  ___  ___  ________  ________  _________   
+|\   ____\|\  \|\  \|\   __  \|\   ____\|\___   ___\ 
+\ \  \___|\ \  \\\  \ \  \|\  \ \  \___|\|___ \  \_| 
+ \ \  \  __\ \   __  \ \  \\\  \ \_____  \   \ \  \  
+  \ \  \|\  \ \  \ \  \ \  \\\  \|____|\  \   \ \  \ 
+   \ \_______\ \__\ \__\ \_______\____\_\  \   \ \__\
+    \|_______|\|__|\|__|\|_______|\_________\   \|__|
+                                 \|_________|   '
+
+
+if [ ! -d "IMPRINTS" ]; then
+  echo "No directory for Neural Imprints exists. Automatically creating..."
+  mkdir "IMPRINTS"
+  read -p "Name your new imprint: " imprint_name
+  touch IMPRINTS/${imprint_name}.ni
+  echo -n '[]' > IMPRINTS/${imprint_name}.ni
+fi
+
+menu(){
+echo "---------------------------Current imprints:---------------------------"
+ls IMPRINTS/*.ni | xargs -n 1 basename | sed -e 's/\.ni$//'
+echo "-----------------------------------------------------------------------"
+
+options=("Create a clean imprints" "Inject an imprint" "Wipe an imprint" "Exit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Create a clean imprints")
+            read -p "Name your new imprint: " imprint_name
+            touch IMPRINTS/${imprint_name}.ni
+            echo -n '[]' > IMPRINTS/${imprint_name}.ni
+            menu;;
+
+        "Inject an imprint")
+            read -p "Inject imprint: " imprint_name
+            python ghost.py $imprint_name
+            menu;;
+
+        "Wipe an imprint")   
+            read -p "Wipe imprint: " imprint_name
+            rm IMPRINTS/${imprint_name}.ni
+            menu
+            ;;
+
+        "Exit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+}
+
+menu
+
+
+# read -p "Inject imprint: " imprint_name
+
+# python ghost.py $imprint_name
+
+
+
