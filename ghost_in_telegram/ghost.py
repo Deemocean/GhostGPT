0765a6b9f88a3db6c477da6896db73e876c95bb1
Deemo Chen <yc938@cornell.edu>
2023-03-05 17:24:48 -0500

    Fix telegram config, add multiple img output from /imgc

--- ghost_in_telegram/ghost.py
+++ ghost_in_telegram/ghost.py
@@ -1,45 +1,39 @@
 import openai
 import sys
 import logging
-from telegram import Update
+from telegram import Update, InputMediaPhoto
 from telegram.constants import ParseMode
 from telegram.helpers import escape_markdown
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 
-#Change these lines if you don't want to use the config file.
-key = "" #openAI key
-tt = "" #Telegram token
-
-
-#The following code reads the config file if key or tt not set.
-#---------------------------------------------------------------------------------------
+import json
 #Get options.
-if key =="" or tt == "":
-    import json
-    data_file_path = '../config.txt'
-    config_dict = {}
-    try:
-        with open (data_file_path) as config:
-            config_dict = json.load(config)
-    except (FileNotFoundError, json.decoder.JSONDecodeError):
-        print("No valid config file found!\nRun either the shell script or the python script.")
-        exit()
+config_dict = {}
+data_file_path = '../config.json'
+try:
+    with open (data_file_path) as config:
+        config_dict = json.load(config)
+except (FileNotFoundError, json.decoder.JSONDecodeError):
+    print("No valid config file found!\nRun either the shell script or the python script.")
+    exit()
 
-    try:
-        key = config_dict["TELEGRAM_TOKEN"]
-    except KeyError:
-        print("No Telegram token found!")
-        exit()
+key = ""
+tg_token=""
 
-    try:
-        key = config_dict["API_KEY"]
-    except KeyError:
-        print("No openAI token found!")
-        exit()
-    
-#---------------------------------------------------------------------------------------
+try:
+    key = config_dict["API_KEY"]
+except KeyError:
+    print("No openAI key found!")
+    exit()
+
+try:
+    tg_token = config_dict["TELEGRAM_TOKEN"]
+except KeyError:
+    print("No Telegram token found!")
+    exit()
+
 openai.api_key = key
-TOKEN = tt
+TOKEN = tg_token
 
 TOKEN_REQUEST_LIMIT = 4096
 token_outbound_count = 0
@@ -118,17 +112,21 @@ async def gst(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def imgc(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     usr_input = update.effective_message.text[5:]
-    try:
-        response = openai.Image.create(
-            prompt=usr_input,
-            n=1,
-            size="1024x1024"
-            )
-        resp = response['data'][0]['url']
-    except:
-        resp = "error :(..."
-   
-    await context.bot.send_photo(chat_id=update.effective_chat.id, photo=resp)
+    #try:
+    response = openai.Image.create(
+        prompt=usr_input,
+        n=4,
+        size="1024x1024"
+        )
+    img0 = InputMediaPhoto(media=response['data'][0]['url'])
+    img1 = InputMediaPhoto(media=response['data'][1]['url'])
+    img2 = InputMediaPhoto(media=response['data'][2]['url'])
+    img3 = InputMediaPhoto(media=response['data'][3]['url'])
+
+    await context.bot.send_media_group(chat_id=update.effective_chat.id, media=[img0,img1,img2,img3])
+    #except:
+     #   await context.bot.send_message(chat_id=update.effective_chat.id, text="error :(...")
+    #await context.bot.send_photo(chat_id=update.effective_chat.id, photo=resp)
 
 # async def imgm(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
